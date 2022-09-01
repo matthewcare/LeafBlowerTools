67cef69f67a08c22d91078c847241e395a3b18ac
Matthew Care <matty_care@hotmail.co.uk>
2022-09-01 19:05:23 +0200

    Add menu for boss farmer
    Menus for boss farming options

--- src/Lbr.Tools.Core/Constants/Menus.cs
+++ src/Lbr.Tools.Core/Constants/Menus.cs
@@ -4,7 +4,8 @@ public partial class Constants
 {
     public class Menus
     {
-        public static readonly string[] MainMenu = { "Trader", "Exit" };
+        public static readonly string[] MainMenu = { "Trader", "Boss Farmer", "Exit" };
         public static readonly string[] TradeMenu = { "Gems", "Main Menu" };
+        public static readonly string[] BossFarmMenu = { "Cycle", "Cycle with Vortex", "Main Menu" };
     }
 }
--- src/Lbr.Tools.Services/MenuService/MenuService.cs
+++ src/Lbr.Tools.Services/MenuService/MenuService.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using Lbr.Tools.Core.Constants;
 using Lbr.Tools.Core.Entities;
+using Lbr.Tools.Services.BossFarmService;
 using Lbr.Tools.Services.TradeService;
 
 namespace Lbr.Tools.Services.MenuService;
@@ -8,10 +9,12 @@ namespace Lbr.Tools.Services.MenuService;
 public class MenuService : IMenuService
 {
     private readonly ITradeService _tradeService;
+    private readonly IBossFarmService _bossFarmService;
 
-    public MenuService(ITradeService tradeService)
+    public MenuService(ITradeService tradeService, IBossFarmService bossFarmService)
     {
         _tradeService = tradeService;
+        _bossFarmService = bossFarmService;
     }
 
     public void ShowMainMenu()
@@ -26,6 +29,9 @@ public class MenuService : IMenuService
         {
             case "Trader":
                 ShowTradeMenu();
+                break;
+            case "Boss Farmer":
+                ShowBossFarmerMenu();
                 break;
             case "Exit":
                 Environment.Exit(0);
@@ -48,6 +54,33 @@ public class MenuService : IMenuService
         {
             case "Gems":
                 _tradeService.StartTrading(new Gem());
+                ShowMainMenu();
+                break;
+            case "Main Menu":
+                ShowMainMenu();
+                break;
+            default:
+                ShowTradeMenu();
+                break;
+        }
+    }
+
+    public void ShowBossFarmerMenu()
+    {
+        string? menuOption = null;
+        while (menuOption is null)
+        {
+            menuOption = GetMenuOption(Constants.Menus.BossFarmMenu);
+        }
+
+        switch (menuOption)
+        {
+            case "Cycle":
+                _bossFarmService.StartFarming(false);
+                ShowMainMenu();
+                break;
+            case "Cycle with Vortex":
+                _bossFarmService.StartFarming(true);
                 ShowMainMenu();
                 break;
             case "Main Menu":

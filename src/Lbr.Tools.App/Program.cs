d653752061348edf11114aacbf889bd3efd8f5c3
Matthew Care <matty_care@hotmail.co.uk>
2022-09-01 19:03:52 +0200

    Add a window title
    Add a proper window title

--- src/Lbr.Tools.App/Program.cs
+++ src/Lbr.Tools.App/Program.cs
@@ -2,6 +2,7 @@
 using Lbr.Tools.Services.MenuService;
 using Microsoft.Extensions.DependencyInjection;
 
+Console.Title = "Leaf Blower Revolution Tools";
 DependencyInjection.RegisterServices();
 var menuService = DependencyInjection.ServiceProvider.GetService<IMenuService>() ?? throw new Exception("Menu service was null");
 menuService.ShowMainMenu();

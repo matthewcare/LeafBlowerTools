0f33c263056411aa21b816f9ca697dbf9b636136
Matthew Care <matty_care@hotmail.co.uk>
2022-09-01 19:03:30 +0200

    Add more input helpers
    Trigger keypresses and scroll to bottom

--- src/Lbr.Tools.Services/InputService/IInputService.cs
+++ src/Lbr.Tools.Services/InputService/IInputService.cs
@@ -1,6 +1,10 @@
-﻿namespace Lbr.Tools.Services.InputService;
+﻿using Dapplo.Windows.Input.Enums;
+
+namespace Lbr.Tools.Services.InputService;
 
 public interface IInputService
 {
     void LeftClickAtPoint(int x, int y, int screen = 0);
+    void ScrollToBottom();
+    void TriggerKeyPress(VirtualKeyCode key);
 }
--- src/Lbr.Tools.Services/InputService/InputService.cs
+++ src/Lbr.Tools.Services/InputService/InputService.cs
@@ -1,5 +1,6 @@
 ﻿using Dapplo.Windows.Common.Structs;
 using Dapplo.Windows.Input.Enums;
+using Dapplo.Windows.Input.Keyboard;
 using Dapplo.Windows.Input.Mouse;
 
 namespace Lbr.Tools.Services.InputService;
@@ -9,14 +10,30 @@ public class InputService : IInputService
     public void LeftClickAtPoint(int x, int y, int screen = 0)
     {
         x *= (screen + 1);
-
-        // Upstream issue means you can't use MouseInputGenerator.MouseClick because it
-        // does a MouseDown twice instead of MouseDown and then MouseUp
         MouseInputGenerator.MouseDown(MouseButtons.Left, new NativePoint(x, y));
-        // Mousedown registers at between 40 and 100 ms.
+        // Game registers at between 40 and 100 ms.
         // Using upper bounds for safety
         Thread.Sleep(100);
         MouseInputGenerator.MouseUp(MouseButtons.Left, new NativePoint(x, y));
+        Thread.Sleep(100);
+    }
+
+    public void ScrollToBottom()
+    {
+        for (var i = 0; i < 20; i++)
+        {
+            MouseInputGenerator.MoveMouseWheel(-1);
+            Thread.Sleep(100);
+        }
+    }
+
+    public void TriggerKeyPress(VirtualKeyCode key)
+    {
+        KeyboardInputGenerator.KeyDown(key);
+        // Game registers at between 40 and 100 ms.
+        // Using upper bounds for safety
+        Thread.Sleep(100);
+        KeyboardInputGenerator.KeyUp(key);
         Thread.Sleep(100);
     }
 }

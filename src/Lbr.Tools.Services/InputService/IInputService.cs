f719a9ff33e54e1d6544806c48695e4a87e44ca2
Matthew Care <matty_care@hotmail.co.uk>
2022-09-25 20:40:07 +0200

    Add more inputs to handle more things
    Refactor more input based things into the input service

--- src/Lbr.Tools.Services/InputService/IInputService.cs
+++ src/Lbr.Tools.Services/InputService/IInputService.cs
@@ -1,6 +1,14 @@
-﻿namespace Lbr.Tools.Services.InputService;
+﻿using Dapplo.Windows.Input.Enums;
+
+namespace Lbr.Tools.Services.InputService;
 
 public interface IInputService
 {
     void LeftClickAtPoint(int x, int y, int screen = 0);
+    void ScrollDown(int triggers);
+    void ScrollUp(int triggers);
+    void ScrollToTop();
+    void ScrollToBottom();
+    void TriggerKeyPress(VirtualKeyCode key);
+    void TriggerKeyPress(ConsoleKey key);
 }
--- src/Lbr.Tools.Services/InputService/InputService.cs
+++ src/Lbr.Tools.Services/InputService/InputService.cs
@@ -1,5 +1,6 @@
 ﻿using Dapplo.Windows.Common.Structs;
 using Dapplo.Windows.Input.Enums;
+using Dapplo.Windows.Input.Keyboard;
 using Dapplo.Windows.Input.Mouse;
 
 namespace Lbr.Tools.Services.InputService;
@@ -9,14 +10,48 @@ public class InputService : IInputService
     public void LeftClickAtPoint(int x, int y, int screen = 0)
     {
         x *= (screen + 1);
-
-        // Upstream issue means you can't use MouseInputGenerator.MouseClick because it
-        // does a MouseDown twice instead of MouseDown and then MouseUp
         MouseInputGenerator.MouseDown(MouseButtons.Left, new NativePoint(x, y));
-        // Mousedown registers at between 40 and 100 ms.
+        // Game registers at between 40 and 100 ms.
         // Using upper bounds for safety
         Thread.Sleep(100);
         MouseInputGenerator.MouseUp(MouseButtons.Left, new NativePoint(x, y));
         Thread.Sleep(100);
     }
+
+    public void ScrollDown(int triggers)
+    {
+        for (var i = 0; i < triggers; i++)
+        {
+            MouseInputGenerator.MoveMouseWheel(-1);
+            Thread.Sleep(10);
+        }
+    }
+    public void ScrollUp(int triggers)
+    {
+        for (var i = 0; i < triggers; i++)
+        {
+            MouseInputGenerator.MoveMouseWheel(1);
+            Thread.Sleep(10);
+        }
+    }
+
+    public void ScrollToBottom() => ScrollDown(20);
+
+    public void ScrollToTop() => ScrollUp(20);
+
+    public void TriggerKeyPress(VirtualKeyCode key)
+    {
+        KeyboardInputGenerator.KeyDown(key);
+        // Game registers at between 40 and 100 ms.
+        // Using upper bounds for safety
+        Thread.Sleep(100);
+        KeyboardInputGenerator.KeyUp(key);
+    }
+
+
+    public void TriggerKeyPress(ConsoleKey key)
+    {
+        TriggerKeyPress((VirtualKeyCode)key);    
+    }
+
 }

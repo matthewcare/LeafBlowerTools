1386474cb5e395c421da23260ea70a8e7b2616a3
Matthew Care <matty_care@hotmail.co.uk>
2022-09-25 20:40:43 +0200

    Add hotkey constants
    Now that artifacts are keybindable, add a new file to centralise

---
+++ src/Lbr.Tools.Core/Constants/HotKeys.cs
@@ -0,0 +1,15 @@
+﻿namespace Lbr.Tools.Core.Constants;
+
+public partial class Constants
+{
+    public class HotKeys
+    {
+        public const ConsoleKey BlazingSkull = ConsoleKey.F1;
+        public const ConsoleKey Wind = ConsoleKey.F2;
+        public const ConsoleKey Vortex = ConsoleKey.F3;
+
+
+        public const ConsoleKey AreasMenu = ConsoleKey.V;
+        public const ConsoleKey CardsMenu = ConsoleKey.P;
+    }
+}
